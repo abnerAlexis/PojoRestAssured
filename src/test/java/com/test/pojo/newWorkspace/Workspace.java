a422fa179e3915b76f2004367babf5f932fb44f2
Alexis Abner <alexisjuliaabner@gmail.com>
2023-01-10 10:01:22 -0700

    Add @JsonInclude(JsonInclude.NON_Null)

--- src/test/java/com/test/pojo/newWorkspace/Workspace.java
+++ src/test/java/com/test/pojo/newWorkspace/Workspace.java
@@ -1,5 +1,8 @@
 package com.test.pojo.newWorkspace;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Workspace {
     private String id;
     private String name;
